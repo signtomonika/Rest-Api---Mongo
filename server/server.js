a017fd994ec4f590979d6f923fe9a016950af019
monika ravi <monikar@hawsco.com>
2018-10-01 15:26:52 -0700

    heroku change

--- server/server.js
+++ server/server.js
@@ -8,6 +8,8 @@ var { User } = require('./models/user');
 
 var app = express();
 
+const port = process.env.PORT || 3000;
+
 app.use(bodyParser.json()); //using the return value of bodyParser.json() as middleware
 
 app.post('/todos', (req, res) => {
@@ -66,8 +68,8 @@ app.get('/todos/:id', (req, res) => {
 
 });
 
-app.listen(3000, () => {
-    console.log('Server started on port 3000..');
+app.listen(port, () => {
+    console.log(`Server started at port ${port}..`);
 });
 
 
