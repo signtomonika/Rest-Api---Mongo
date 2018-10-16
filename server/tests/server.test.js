d18ccbe53c8c2ada287978fa9412628582fa00e5
monika ravi <monikar@hawsco.com>
2018-10-16 16:01:32 -0700

    GET/todos/:id Test Script added

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,14 +1,17 @@
 const expect = require('expect');
 const superTest = require('supertest');
+const { ObjectID } = require('mongodb');
 
 const { app } = require('../server');
 const { Todo } = require('../models/todo');
 
 const todos = [
     {
+        _id: new ObjectID(),
         text: 'test todo 1'
     },
     {
+        _id: new ObjectID(),
         text: 'test todo 2'
     }
 ];
@@ -44,7 +47,7 @@ describe('POST/todos', () => {
                     return done(err);   //check and return if any error
                 }
 
-                Todo.find({text}).then(   //executes when post succeeds
+                Todo.find({ text }).then(   //executes when post succeeds
                     (todos) => {
                         expect(todos.length).toBe(1);   //check if todos length is 1 (if todos is empty)
                         expect(todos[0].text).toBe(text);  //check if the text present is same as test text
@@ -92,17 +95,53 @@ describe('POST/todos', () => {
 
 });
 
-describe('GET/todos',()=>{
+describe('GET/todos', () => {
 
-    it('should get all todos',(done)=>{
+    it('should get all todos', (done) => {
 
         superTest(app)
             .get('/todos')
             .expect(200)
-            .expect((res)=>{
+            .expect((res) => {
                 expect(res.body.todos.length).toBe(2);
             })
             .end(done);
+
+    });
+
+});
+
+describe('GET/todos/:id', () => {
+
+    it('should return todo doc', (done) => {
+
+        superTest(app)
+            .get(`/todos/${todos[0]._id.toHexString()}`)
+            .expect(200)
+            .expect(
+                (res) => {
+                    expect(res.body.todo.text).toBe(todos[0].text);
+                })
+            .end(done);
+    });
+
+    it('should return 404 if todo not found',(done)=>{
+
+        var id = new ObjectID().toHexString();
+
+        superTest(app)
+            .get(`/todos/${id}`)
+            .expect(404)
+            .end(done);
+
+    });
+
+    it('should return 400 for non-object ids', (done)=>{
+
+        superTest(app)
+        .get('/todos/123abc')
+        .expect(400)
+        .end(done);
 
     });
 
