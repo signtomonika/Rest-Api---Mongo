1c83b86252d9ef97349d2a4adeaeecc9cf2ad4a4
monika ravi <monikar@hawsco.com>
2018-10-17 09:02:55 -0700

    DELETE Todos by ID Test Script Added

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -146,3 +146,63 @@ describe('GET/todos/:id', () => {
     });
 
 });
+
+describe('DELETE/todos/:id',()=>{
+
+    it('should remove a todo',(done)=>{
+
+        var id = todos[1]._id.toHexString();
+
+        superTest(app)
+            .delete(`/todos/${id}`)
+            .expect(200)
+            .expect(
+                (res)=>{
+
+                    expect(res.body.todo._id).toBe(id);
+
+                })
+                .end(
+                    (err,res)=>{
+                        if(err){
+                            return done(err);
+                        }
+
+                        Todo.findById(id)
+                            .then(
+
+                                (todo)=>{
+                                   expect(todo).toNotExist();
+                                   done();
+                                }
+
+                            ).catch((err)=>{
+                                done(err);
+                            })
+
+                    })
+
+    });
+
+    it('should return 404 if todo not found',(done)=>{
+
+        var id = new ObjectID().toHexString();
+
+        superTest(app)
+            .delete(`/todos/${id}`)
+            .expect(404)
+            .end(done);
+
+
+    });
+
+    it('should return 400 if object id is invalid',(done)=>{
+
+        superTest(app)
+        .delete('/todos/123abc')
+        .expect(400)
+        .end(done);
+
+    });
+
+})
