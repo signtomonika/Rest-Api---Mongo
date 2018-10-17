3cf79f0a720e8ab2eb710e5713c123a84c7057ad
monika ravi <monikar@hawsco.com>
2018-10-17 10:25:43 -0700

    modified to loacl

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;  //config mongoose to use the Promises in node and not other 3rd party promises
 
-mongoose.connect(process.env.MONGODB_URI);  //herokumongolab addon lives in the process.env
+//mongoose.connect(process.env.MONGODB_URI);  //herokumongolab addon lives in the process.env
 
-//mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {
 
